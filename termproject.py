9b2fd3f90f8c2d0a2c5430554b15dea1ecc8153a
sshingade2002 <35470313+sshingade2002@users.noreply.github.com>
2020-11-29 00:11:07 -0800

    Add files via upload

---
+++ termproject.py
@@ -0,0 +1,22 @@
+import math, copy, random
+from cmu_112_graphics import *
+
+
+def playGame():
+    margin = 25
+    height = 700+2*margin
+    width = 700+2*margin
+    runApp(width=width, height=height, title='Game') 
+
+#################################################
+# main
+#################################################
+
+def main():
+    playGame()
+
+if __name__ == '__main__':
+    main()
+
+
+
